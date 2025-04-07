4f6d75805e92472a12c360b1b92c72e734b9092f
Freddy <freddy_maupou@orange.fr>
2025-04-07 12:05:38 +0200

    [FIX] mail

--- lib/resend.ts
+++ lib/resend.ts
@@ -4,11 +4,11 @@ import { Resend } from "resend";
 import EmailTemplate from "@/app/template/email/page"
 
 const resend = new Resend(process.env.RESEND_API_KEY);
-const myMail = process.env.MY_MAIL || [];
+// const myMail = process.env.MY_MAIL || [];
 
 export const sendEmail  = async (mail : string, subject : string, message : string) => {
     const send = await resend.emails.send({
-        to: myMail, // list of receivers
+        to: "freddy_maupou@orange.fr", // list of receivers
         from: "'Nouvelle demande de contact !' <onboarding@resend.dev>", // dev adress => will work once deployed?
         subject: "Quelqu'un me contacte !", // Subject line
         react: EmailTemplate(mail, subject, message), // html body or React 
